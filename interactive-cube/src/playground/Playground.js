6279a1c6851e88f88682bfad5d295b4614f9f0b7
Ichizo Umehara <iumehara@vmware.com>
2023-10-02 09:30:58 -0400

    Add button to toggle between cube and example

--- interactive-cube/src/playground/Playground.js
+++ interactive-cube/src/playground/Playground.js
@@ -1,11 +1,22 @@
+import { useState } from "react";
 import Canvas from "./Canvas";
-import InputsForm from "./InputsForm";
+import Example from "../example/Example";
 
 const Playground = () => {
+    const [showCube, setShowCube] = useState(true)
+
+    const content = () => {
+        if (showCube) {
+            return <Canvas/>
+        } else {
+            return <Example/>
+        }
+    }
+
     return (
         <>
-            <InputsForm/>
-            <Canvas/>
+            <button onClick={() => setShowCube(!showCube)}>toggle</button>
+            {content()}
         </>
     )
 }
